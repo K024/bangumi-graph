dd1e2dad191d4cf0d0f54424fe2186c2ef7b2517
K024 <Kjy_@live.cn>
2023-11-13 14:46:07 +0800

    add single by id queries

--- Graph/Query.cs
+++ Graph/Query.cs
@@ -10,12 +10,22 @@ public class Query
     public IQueryable<Character> GetCharacters([Service] AppDbContext dbContext)
         => dbContext.Character;
 
+    [UseSingleOrDefault]
+    [UseProjection]
+    public IQueryable<Character> GetCharacterById(long id, [Service] AppDbContext dbContext)
+        => dbContext.Character.Where(c => c.id == id);
+
     [UsePaging]
     [UseProjection]
     [UseFiltering]
     [UseSorting]
     public IQueryable<Episode> GetEpisodes([Service] AppDbContext dbContext)
         => dbContext.Episode;
+
+    [UseSingleOrDefault]
+    [UseProjection]
+    public IQueryable<Episode> GetEpisodeById(long id, [Service] AppDbContext dbContext)
+        => dbContext.Episode.Where(e => e.id == id);
 
     [UsePaging]
     [UseProjection]
@@ -24,10 +34,20 @@ public class Query
     public IQueryable<Subject> GetSubjects([Service] AppDbContext dbContext)
         => dbContext.Subject;
 
+    [UseSingleOrDefault]
+    [UseProjection]
+    public IQueryable<Subject> GetSubjectById(long id, [Service] AppDbContext dbContext)
+        => dbContext.Subject.Where(s => s.id == id);
+
     [UsePaging]
     [UseProjection]
     [UseFiltering]
     [UseSorting]
     public IQueryable<Person> GetPersons([Service] AppDbContext dbContext)
         => dbContext.Person;
+
+    [UseSingleOrDefault]
+    [UseProjection]
+    public IQueryable<Person> GetPersonById(long id, [Service] AppDbContext dbContext)
+        => dbContext.Person.Where(p => p.id == id);
 }
